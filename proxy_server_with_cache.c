44aad47523c94b8093d798ff8ed5e01b794285dc
sanjanabharath <sanjanabarath@gmail.com>
2025-11-04 22:04:22 +0530

    Added server

--- proxy_server_with_cache.c
+++ proxy_server_with_cache.c
@@ -1,7 +1,22 @@
 #include "proxy_parse.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <time.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <time.h>
+
+#define MAX_CLIENTS 10
 
 typedef struct cache_element cache_element;
 
@@ -13,3 +28,28 @@ struct cache_element{
     cache_element* next;
 };
 
+int port_number = 8080;
+int proxy_socketId;
+pthread_t tid[MAX_CLIENTS];
+sem_t semaphore;
+pthread_mutex_t lock;
+
+cache_element* head;
+int cache_size;
+
+int main(int argc, char* argv[]) {
+    int client_socketId, client_len;
+    struct sockaddr server_addr, client_addr;
+    sem_init(&semaphore, 0, MAX_CLIENTS);
+    pthread_mutex_init(&lock, NULL);
+    if (argv == 2)
+    {
+        port_number = atoi(argv[1]);
+    } else {
+        printf("Too few arguments");
+        exit(1);
+    }
+
+    printf("Starting proxy server at port: %d\n", port_number);
+    
+}
